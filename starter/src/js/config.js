a4581c8206d0f8d89257eddad98b6c47da8a5039
sjerzak <slawomir.jerzak@gmail.com>
2021-07-08 09:16:00 +0200

    first commit

---
+++ starter/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7f040556-0fc3-4b50-b9c5-f39699abccd9';
+export const MODAL_CLOSE_SEC = 2.5;
